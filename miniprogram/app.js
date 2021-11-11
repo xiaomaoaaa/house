1451e85919efc28c3f8dcd4102cb9bf5c8d1826b
PC\XT <abelmao@shinyway.com.cn>
2021-11-11 10:55:33 +0800

    没修改钱，可以跑通的代码！

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -6,7 +6,7 @@ App({
             console.error('请使用 2.2.3 或以上的基础库以使用云能力')
         } else {
             wx.cloud.init({
-                env: 'dev-house-0tiax',
+                env: 'cloud1-2g7hsrjqa5c1a788',
                 traceUser: true,
             })
         }
